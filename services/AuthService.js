6870ea5c74e8416a5efdfb535f864e9c3b96ec99
Joe Strong <joestrong@gmail.com>
2019-03-10 17:56:42 +0000

    Exchanges facebook token for app token

--- services/AuthService.js
+++ services/AuthService.js
@@ -15,9 +15,35 @@ export default class AuthService {
 
     if (type === 'success' && token) {
       homeScreen.setState({status: 'Logging in...'});
+      let appToken;
+      try {
+        appToken = await this.exchangeFacebookToken(token);
+        console.log(appToken);
+      } catch (e) {
+        homeScreen.setState({status: e.message});
+      }
+      homeScreen.setState({status: 'Logged in!'});
     }
     if (type === 'cancel') {
       homeScreen.setState({status: 'Could not log in with Facebook'});
     }
+  }
+
+  static async exchangeFacebookToken(fbToken) {
+    const apiHost = 'http://localhost';
+    const response = await fetch(apiHost + '/auth/exchange/facebook', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        token: fbToken
+      })
+    });
+
+    const responseJson = await response.json();
+
+    return responseJson.token;
   }
 }
