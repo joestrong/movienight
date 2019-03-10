087310219230762a12c8f8a42fc6331a952a38ae
Joe Strong <joestrong@gmail.com>
2019-03-10 18:44:44 +0000

    Installs redux

---
+++ actions/Actions.js
@@ -0,0 +1,8 @@
+const actions = {
+  LOGIN: 'LOGIN',
+  LOGIN_PROGRESS: 'LOGIN_PROGRESS',
+  LOGIN_FAILURE: 'LOGIN_FAILURE',
+  LOGOUT: 'LOGOUT'
+}
+
+export default actions
---
+++ reducers/AppReducer.js
@@ -0,0 +1,41 @@
+import Actions from '../actions/Actions';
+import { combineReducers } from 'redux';
+
+const INITIAL_STATE = {
+  user: null,
+  messages: {
+    loginStatus: 'Logged Out'
+  }
+};
+
+const messagesReducer = (state = INITIAL_STATE, action) => {
+  const messages = {...state.messages};
+
+  switch (action.type) {
+    case Actions.LOGIN:
+      messages.loginStatus = 'Logged in';
+      break;
+    case Actions.LOGIN_PROGRESS:
+      messages.loginStatus = 'Logging in..';
+      break;
+    case Actions.LOGIN_FAILURE:
+      messages.loginStatus = 'Login failed';
+      break;
+    case Actions.LOGOUT:
+      messages.loginStatus = 'Logged out';
+      break;
+  }
+
+  return messages;
+};
+
+const userReducer = (state = INITIAL_STATE, action) => {
+  const user = {...state.user};
+
+  return user;
+};
+
+export default combineReducers({
+  messages: messagesReducer,
+  user: userReducer,
+});
